ff984c46714904e458aa357c3d25e1be2b7cad50
Birk Scheben <birkscheben@Air-von-Birk.fritz.box>
2025-02-08 19:15:04 +0100

    pro google

--- klausur.py
+++ klausur.py
@@ -17,9 +17,9 @@ os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
 
 # Initialize the large language model (LLM).  Here, Google's Gemini is used.
-# llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp")
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-pro")
 # The following line is commented out, indicating that OpenAI's GPT model is an alternative that could be used instead.
-llm = ChatOpenAI(model="gpt-4o-2024-08-06")
+# llm = ChatOpenAI(model="gpt-4o-2024-08-06")
 
 #test
 # Access Streamlit's session state.  Session state allows data to persist across multiple runs of the app.
